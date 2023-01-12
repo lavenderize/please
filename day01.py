7b6fdf28ec852c3224caaf28a7edef277ede74f9
lavenderize <liexquisit@gmail.com>
2023-01-12 16:30:30 +0900

    GUI 04

--- day01.py
+++ day01.py
@@ -1,28 +1,7 @@
-# 교재 dictionary 예제
+import tkinter as tk
 
+win = tk.Tk()
+win.geometry('400x300')
+win.title('Sleepy')
+win.mainloop()
 
-# quotes = {
-#     "Moe": "A wise guy, huh?",
-#     "Larry": "Ow!",
-#     "Curly": "Nyuk nyuk!", # key : value
-#     }
-# stooge = "Curly"
-# print(stooge, "says:", quotes[stooge])
-
-subjects = {
-    'Eng': 'A+',
-    'His': 'A0',
-    'Sci': 'B+'
-           }
-student = 'Jane'
-subject = 'Sci'
-print(student, "'s", subject, "grade is", subjects[subject], '.')
-
-#old style
-print("%s s %s grade is %s ." % (student, subject, subjects[subject]))
-
-#format func
-print("{0} s {1} grade is {2}.".format(student, subject, subjects[subject])) # key positioning possibility
-
-#fString
-print(f'{student}s {subject} grade is {subjects[subject]}.')
