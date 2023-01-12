adfbfffbdcd47fb7dab615d9401c2ec2a77de00c
lavenderize <liexquisit@gmail.com>
2023-01-12 16:37:40 +0900

    temperature 01

--- day01.py
+++ day01.py
@@ -1,6 +1,3 @@
-import tkinter as tk
-
-win = tk.Tk()
-win.geometry('400x300')
-win.title('Sleepy')
-win.mainloop()
+fahrenheit = float(input('화씨 온도 : '))
+celsius = (fahrenheit - 32.0) * (5.0 / 9.0)
+print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다')
