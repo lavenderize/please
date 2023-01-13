fa306c6f61076df37d0e041f0e174d39faa5ebf7
lavenderize <liexquisit@gmail.com>
2023-01-13 15:52:04 +0900

    day02_mission_02

---
+++ day02_mission.py
@@ -0,0 +1,17 @@
+#4.2
+
+small = True
+green = True
+
+if small:
+    if green:
+        print("It's a pea")
+    else:
+        print("It's a cherry")
+
+else:
+    if green:
+        print("It's a watermelon")
+    else:
+        print("It's pumpkin")
+
