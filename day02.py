5db26149634cff2e2f0e6be395deb133bcdaeb36
lavenderize <liexquisit@gmail.com>
2023-01-13 15:22:17 +0900

    day02_if

--- day02.py
+++ day02.py
@@ -1,2 +1,36 @@
-PI = 3.14
-print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다.')
+limits = 20
+tweets = "pass\n" * 6
+
+diff = limits - len(tweets)
+
+if diff >= 0:
+    print(tweets)
+
+else:
+    print(f'제한 글자 수 {abs(diff)} 초과') # 절대값으로 변환
+
+############################
+
+
+vowels = 'aeiou'
+
+letter = 'x'
+
+if letter in vowels:
+    print("fail")
+
+else:
+    print("success")
+
+
+########################
+
+letter = 'u'
+
+if letter not in vowels:
+    print("success")
+
+else:
+    print("fail")
+
+
